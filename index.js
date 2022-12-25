a905ce311e77cb58aba9d6f7fde84abf7fcc5f96
ligotommy <116342243+ligotommy@users.noreply.github.com>
2022-12-25 17:02:06 +0200

    added buttons to move

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 let step = 7
 let dx = 0
+let shirtDy = 0
+let movingLeft = false
+let buttonLeft = false
+let buttonRight = false
+let movingRight = false
 
 function moveSuitCase(where) {
     let x = document.getElementById("suit-case").offsetLeft
+    if (where === "left" && x>0) {
+        x -= step
+        document.getElementById("suit-case").style.left = `${x}px`
+    } else if (where === "right" && x<1230) {
+        x += step
+        document.getElementById("suit-case").style.left = `${x}px`
+    }
+}
+
+function moveRight() {
+    if (buttonRight == false) {
+        buttonRight = true
+        dx+=1
+        console.log(dx)
+    }
+}
+
+function stopRight() {
+    if (buttonRight) {
+        buttonRight = false
+        dx-=1
+        console.log(dx)
+    }
+}
+
+function moveLeft() {
+    if (buttonRight == false) {
+        buttonLeft = true
+        dx-=1
+        console.log(dx)
+    }
+}
+
+function stopLeft() {
+    if (buttonLeft) {
+        buttonLeft = false
+        dx+=1
+        console.log(dx)
+    }
+}
+
+function moveShirt(where) {
+    let x = document.getElementById("shirt").offsetTop
     if (where === "left" && x>-10) {
         x -= step
         document.getElementById("suit-case").style.left = `${x}px`
@@ -10,32 +58,36 @@ function moveSuitCase(where) {
         x += step
         document.getElementById("suit-case").style.left = `${x}px`
     }
-    console.log(x)
 }
 
 addEventListener('keydown', (event) => {
-    if (event.key == "a" || event.key == "A" || event.key == "ArrowLeft") {
-        dx=-1
-    } else if (event.key == "d" || event.key == "D" || event.key == "ArrowRight") {
-        dx=1
+    if ((event.key == "a" || event.key == "A" || event.key == "ArrowLeft") && movingLeft==false) {
+        movingLeft = true
+        dx-=1
+    } else if ((event.key == "d" || event.key == "D" || event.key == "ArrowRight") && movingRight==false) {
+        movingRight = true
+        dx+=1
     }
-    console.log(event.key)
+    console.log(dx)
 });
 
 addEventListener('keyup', (event) => {
-    dx=0
-    // if (event.key == "a" || event.key == "A" || event.key == "ArrowLeft") {
-    //     dx=-1
-    // } else if (event.key == "d" || event.key == "D" || event.key == "ArrowRight") {
-    //     dx=1
-    // }
+    // dx=0
+    if (event.key == "a" || event.key == "A" || event.key == "ArrowLeft") {
+        movingLeft = false
+        dx+=1
+    } else if (event.key == "d" || event.key == "D" || event.key == "ArrowRight") {
+        movingRight = false
+        dx-=1
+    }
+    console.log(dx)
 });
 
 function fun() {
-    if (dx == 1) {
+    if (dx >= 1) {
         moveSuitCase("right")
     }
-    if (dx == -1) {
+    if (dx <= -1) {
         moveSuitCase("left")
     }
     requestAnimationFrame(fun)
